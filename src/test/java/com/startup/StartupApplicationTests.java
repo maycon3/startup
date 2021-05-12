d981d23d8f795449d7ce287b0e812be91d280949
maycon3 <maycon.trb@gmail.com>
2021-05-12 15:06:57 -0300

    primeiro commit

---
+++ src/test/java/com/startup/StartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
